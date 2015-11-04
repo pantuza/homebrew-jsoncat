553d8931c2b7f2815349e62ab9e288c5ae2c79d1
gustavo.pantuza <gustavo.pantuza@corp.globo.com>
2015-11-04 13:13:26 -0200

    Created the Jsoncat Homebrew formula

---
+++ Formula/jsoncat.rb
@@ -0,0 +1,31 @@
+# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                /usr/local/Library/Contributions/example-formula.rb
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Jsoncat < Formula
+  desc ""
+  homepage "https://github.com/pantuza/jsoncat"
+  url "https://github.com/pantuza/jsoncat/blob/master/build/home:pantuza/jsoncat/jsoncat-0.1.0.tar.gz"
+  version "0.1.0"
+  sha256 "42f27901be0c67b17b45d1ec55a5a61c100a11ad07b9b5f28ee1886835f99bb6"
+
+  depends_on "cmake" => :build
+
+  def install
+    system "make", "install" # if this fails, try separate make/make install steps
+    
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test jsoncat`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
