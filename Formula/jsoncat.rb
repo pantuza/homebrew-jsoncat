be74e8322ca060f2612886d6334f73834c85a762
Gustavo Pantuza Coelho Pinto <gustavo.pantuza@corp.globo.com>
2016-07-13 16:33:39 -0300

    Updating tarball sha256 for jsoncat 0.1.0

--- Formula/jsoncat.rb
+++ Formula/jsoncat.rb
@@ -5,16 +5,16 @@
 class Jsoncat < Formula
   desc ""
   homepage "https://github.com/pantuza/jsoncat"
-  url "https://github.com/pantuza/jsoncat/raw/master/build/home:pantuza/jsoncat/jsoncat-0.1.0.tar.gz"
-  version "0.1.0"
-  sha256 "6f431da5064a25a39417d8b3b76deba24f8c87c007379c23d6e8de795c38398d"
+  url "https://github.com/pantuza/jsoncat/raw/master/build/home:pantuza/jsoncat/jsoncat-0.1.1.tar.gz"
+  version "0.1.1"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 
   depends_on "cmake" => :build
 
   def install
     system "make"
     bin.install "bin/jsoncat"
-    
+
   end
 
   test do
