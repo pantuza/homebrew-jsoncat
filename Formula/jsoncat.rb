471015619ce0de8fbcd7cd3775dc9c5ba9454dfb
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-07-15 11:05:32 -0300

    Added description and tests to jsoncat Formulae

--- Formula/jsoncat.rb
+++ Formula/jsoncat.rb
@@ -1,9 +1,9 @@
-# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
-#                /usr/local/Library/Contributions/example-formula.rb
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Jsoncat < Formula
-  desc ""
+  desc "Json pretty-print parser based on a recursive lexical analyser. " \
+       "The parser was based on the specification defined at json.org. " \
+       "The input file is parsed to build a json object. " \
+       "If the object is correct, it will be pretty-printed to standard output."
+
   homepage "https://github.com/pantuza/jsoncat"
   url "https://github.com/pantuza/jsoncat/raw/master/build/home:pantuza/jsoncat/jsoncat-0.1.2.tar.gz"
   version "0.1.2"
@@ -19,15 +19,6 @@ class Jsoncat < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test jsoncat`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "make test"
   end
 end
