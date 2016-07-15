3142dc9c12d947ed92d0dfef9628a260f2427910
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-07-15 11:47:22 -0300

    Description correcteb by the audit warning. Length should be less than 80 characters

--- Formula/jsoncat.rb
+++ Formula/jsoncat.rb
@@ -1,12 +1,10 @@
 class Jsoncat < Formula
-  desc "Json pretty-print parser based on a recursive lexical analyser. " \
-       "The parser was based on the specification defined at json.org. " \
-       "The input file is parsed to build a json object. " \
-       "If the object is correct, it will be pretty-printed to standard output."
 
+  desc "Json pretty-print parser based on a recursive lexical analyser."
   homepage "https://github.com/pantuza/jsoncat"
-  url "https://github.com/pantuza/jsoncat/raw/master/build/home:pantuza/jsoncat/jsoncat-0.1.2.tar.gz"
   version "0.1.2"
+
+  url "https://github.com/pantuza/jsoncat/raw/master/build/home:pantuza/jsoncat/jsoncat-0.1.2.tar.gz"
   sha256 "301c94d4e6df3077a9d6ad7dcecabc97f376e5fc0df29b3e28b7c51839ee1c86"
 
   depends_on "cmake" => :build
