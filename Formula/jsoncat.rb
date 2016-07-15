07a469cdce1f736d8defe1c9ef38afa97651a41d
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-07-15 13:24:25 -0300

    Audit fix: Version must be placed before hash

--- Formula/jsoncat.rb
+++ Formula/jsoncat.rb
@@ -2,8 +2,8 @@ class Jsoncat < Formula
   desc "Json pretty-print parser based on a recursive lexical analyser."
   homepage "https://github.com/pantuza/jsoncat"
   url "https://github.com/pantuza/jsoncat/raw/master/build/home:pantuza/jsoncat/jsoncat-0.1.2.tar.gz"
-  sha256 "301c94d4e6df3077a9d6ad7dcecabc97f376e5fc0df29b3e28b7c51839ee1c86"
   version "0.1.2"
+  sha256 "301c94d4e6df3077a9d6ad7dcecabc97f376e5fc0df29b3e28b7c51839ee1c86"
 
   depends_on "cmake" => :build
 
