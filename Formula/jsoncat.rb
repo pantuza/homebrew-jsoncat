979ec19879ba1c879cbf3111e68bf1f7e9cb49d5
Gustavo Pantuza <gustavopantuza@gmail.com>
2020-10-05 10:45:27 -0300

    Updates jsoncat tar.gz file hash

--- Formula/jsoncat.rb
+++ Formula/jsoncat.rb
@@ -1,9 +1,9 @@
 class Jsoncat < Formula
   desc "Json pretty-print parser based on a recursive lexical analyser."
   homepage "https://github.com/pantuza/jsoncat"
-  url "https://github.com/pantuza/jsoncat/raw/master/build/home:pantuza/jsoncat/jsoncat-0.7.2.tar.gz"
+  url "https://github.com/pantuza/jsoncat/archive/0.7.2.tar.gz"
   version "0.7.2"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "f1cc1dd90c2e7441c72102fba7792660002502204cbb929cb9281a17699c66ab"
 
   depends_on "cmake" => :build
 
