77818352a5f84c1021721a7f125ba9a3e5164728
Gustavo Pantuza <gustavopantuza@gmail.com>
2020-10-05 10:36:20 -0300

    Updates jsoncat version on homebrew formula

--- Formula/jsoncat.rb
+++ Formula/jsoncat.rb
@@ -1,9 +1,9 @@
 class Jsoncat < Formula
   desc "Json pretty-print parser based on a recursive lexical analyser."
   homepage "https://github.com/pantuza/jsoncat"
-  url "https://github.com/pantuza/jsoncat/raw/master/build/home:pantuza/jsoncat/jsoncat-0.1.2.tar.gz"
-  version "0.1.2"
-  sha256 "301c94d4e6df3077a9d6ad7dcecabc97f376e5fc0df29b3e28b7c51839ee1c86"
+  url "https://github.com/pantuza/jsoncat/raw/master/build/home:pantuza/jsoncat/jsoncat-0.7.2.tar.gz"
+  version "0.7.2"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 
   depends_on "cmake" => :build
 
